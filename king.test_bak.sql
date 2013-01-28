d04bf2458172033f1a419e4208ff44f7fde6150b
kingzone <875061288@163.com>
2013-01-28 15:24:49 +0800

    Change the config files and bak sql scripts file to test export.

--- king.test_bak.sql
+++ king.test_bak.sql
@@ -6,6 +6,8 @@ create table test1 (t1 integer);
 create table "test2.test2"("t2.t2" integer);
 create table "king.test".test3(t3 integer);
 create table "king.test".test4(t4 varchar(10));
+create view testView as select t1 from test1;
+
 
 
 ---  clear tables before testing import 
@@ -14,6 +16,7 @@ drop table test1;
 drop table "test2.test2";
 drop table "king.test".test3;
 drop table "king.test".test4;
+drop view testView ;
 
 
 delete from dbc.CostProfileTypes;
