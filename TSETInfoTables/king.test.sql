82ab0abff7a91b15ff86244898a5cec6bb4f8f53
kingzone <875061288@163.com>
2013-01-28 14:59:26 +0800

    Delete the unnecessary directories.

--- TSETInfoTables/king.test.sql
+++
@@ -1,5 +0,0 @@
-create table t3 as (select * from test3) with no data;
-create table test1 (t1 integer);
-create table "test2.test2"("t2.t2" integer);
-create table "king.test".test3(t3 integer);
-create table "king.test".test4(t4 varchar(10));
--- TSETInfoTables_king.test/king.test.sql
+++
@@ -1,4 +0,0 @@
-create table test1 (t1 integer);;
-create table "test2.test2"("t2.t2" integer);;
-create table "king.test".test3(t3 integer);;
-create table "king.test".test4(t4 varchar(10));;
