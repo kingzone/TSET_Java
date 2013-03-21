7adc64de17d7c6138a36af153fb2550dbf0e923b
kingzone <875061288@163.com>
2013-03-21 11:44:58 +0800

    Generate the DDL of 7 tables of 3 metaDBs.

--- src/com/teradata/qaf/tset/run/XmlReaderRun.java
+++ src/com/teradata/qaf/tset/run/XmlReaderRun.java
@@ -1,6 +1,12 @@
 package com.teradata.qaf.tset.run;
 
+import java.util.Iterator;
+
 import com.teradata.qaf.tset.common.CommonConfig;
+import com.teradata.qaf.tset.pojo.Column;
+import com.teradata.qaf.tset.pojo.MetaDB;
+import com.teradata.qaf.tset.pojo.TSETInfoTables;
+import com.teradata.qaf.tset.pojo.Table;
 import com.teradata.qaf.tset.utils.XMLReader;
 
 public class XmlReaderRun {
@@ -13,6 +19,64 @@ public class XmlReaderRun {
 //		XMLReader xmlReader = new XMLReader("ConfFile_schema.xml");
 		XMLReader xmlReader = new XMLReader(CommonConfig.ConfFile_schema("14.00"));
 		xmlReader.parseXml();
+		
+		genMetaDBDDL();
+		genFormatMetaDBDDL();
+	}
+	
+	public static void genMetaDBDDL() {
+		XMLReader xmlReader = new XMLReader(CommonConfig.ConfFile_schema("13.10"));
+		TSETInfoTables tit = xmlReader.parseXml();
+		Iterator<MetaDB> it1 = tit.getMetaDBList().iterator();
+		while(it1.hasNext()) {
+			MetaDB metaDB = it1.next();
+			Iterator<Table> it2 = metaDB.getTableList().iterator();
+			while(it2.hasNext()) {
+				Table table = it2.next();
+				String tableName = table.getName();
+				String[] s = tableName.split("\\.");
+				//String sql = "CREATE TABLE " + tableName + "(";
+				String sql = "CREATE TABLE " + s[1] + "(";
+				Iterator<Column> it3 = table.getColumnList().iterator();
+				while(it3.hasNext()) {
+					Column col = it3.next();
+					sql += col.getName() + " " + col.getType() + ",";
+				}
+				sql = sql.substring(0, sql.length() - 1);
+				sql += ");";
+				System.out.println(sql);
+			}
+			
+		}
+	}
+	
+	public static void genFormatMetaDBDDL() {
+		XMLReader xmlReader = new XMLReader(CommonConfig.ConfFile_schema("13.10"));
+		TSETInfoTables tit = xmlReader.parseXml();
+		Iterator<MetaDB> it1 = tit.getMetaDBList().iterator();
+		while(it1.hasNext()) {
+			MetaDB metaDB = it1.next();
+			Iterator<Table> it2 = metaDB.getTableList().iterator();
+			while(it2.hasNext()) {
+				Table table = it2.next();
+				String tableName = table.getName();
+				String[] s = tableName.split("\\.");
+				String comment = "/*" + s[1] + "*/";
+				System.out.println(comment);
+				//String sql = "CREATE TABLE " + tableName + "(";
+				String sql = "\"CREATE TABLE " + s[1] + "(\" +\n";
+				Iterator<Column> it3 = table.getColumnList().iterator();
+				while(it3.hasNext()) {
+					Column col = it3.next();
+					sql += "\"	" + col.getName() + " " + col.getType() + ",\" + \n";
+				}
+				sql = sql.substring(0, sql.length() - 6);
+				sql += ");\",";
+				System.out.println(sql);
+				System.out.println();
+			}
+			
+		}
 	}
 
 }
