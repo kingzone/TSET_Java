fcb4d9746169d960314ed1e89182e52ac6be5d78
kingzone <875061288@163.com>
2013-03-28 18:07:30 +0800

    Add sql to extend perm.

--- king.test_bak.sql
+++ king.test_bak.sql
@@ -2,6 +2,8 @@
 
 create database "king.test" from dbc as permanent = 500000;
 
+Modify database kingtest as perm=300000000;
+
 create table test1 (t1 integer);
 create table "test2.test2"("t2.t2" integer);
 create table "king.test".test3(t3 integer);
